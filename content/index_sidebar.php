ca45e8288bf506788868d7bb78c8c1c953f05424
zeidlos <julius@zeidlos.de>
2010-01-25 22:22:43 +0100

    Delicious per @font-face eingebunden

--- content/index_sidebar.php
+++ content/index_sidebar.php
@@ -1,7 +1,7 @@
 			<h2>Mach mit!</h2>
-			<p style="font-size:1.8em;line-height:1.3em;">Du bist Auszubildender in einem 
+			<p style="font-size:1.4em;line-height:1.3em;">Du bist Auszubildender in einem 
 			Medienorientierten Beruf?</p>
-			<p style="font-size:1.4em;line-height:1.3em;">Dann zeige uns Dein Talent und mach mit beim 
+			<p style="font-size:1em;line-height:1.3em;">Dann zeige uns Dein Talent und mach mit beim 
 			Designwettbewerb des Fritz-Hen&szlig;ler F&ouml;rderpreis.</p>
-			<p style="font-size:1.4em;line-height:1.3em;">Es gibt grandiose Preise zu gewinnen.<br />
+			<p style="font-size:1em;line-height:1.3em;">Es gibt grandiose Preise zu gewinnen.<br />
 			Faire Konkurrenz + Dein Fachgebiet  = Deine faire Chance.</p>
--- modules/head.php
+++ modules/head.php
@@ -5,13 +5,13 @@
 	<meta name="keywords" conten="Förderpreis, Mediengestalter, GTA, Fotografen, Fritz-Henßler,	
 	Berufskoleg, Berufsschulen, Ausbildung, Auszubildende" />
 	<link href="./css/style.css" rel="stylesheet" type="text/css" />
-
-	<script src="./js/jquery-1.4.min.js" type="text/javascript"></script>
+<!--	<script src="./js/jquery-1.4.min.js" type="text/javascript"></script>
 	<script src="./js/jquery.scrollTo-1.3.3.js" type="text/javascript"></script>
 	<script src="./js/jquery.localscroll-1.2.5.js" type="text/javascript" charset="utf-8"></script>
 	<script src="./js/jquery.serialScroll-1.2.1.js" type="text/javascript" charset="utf-8"></script>
 	<script src="./js/coda-slider.js" type="text/javascript" charset="utf-8"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" ></script>
+--> 
+   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" ></script>
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.5.3/jquery-ui.min.js" ></script>
     <script type="text/javascript">
     	$(document).ready(function(){
