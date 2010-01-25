34e63b36fadfd48342f42186ac72238ad5e3a4f4
zeidlos <julius@zeidlos.de>
2010-01-25 21:11:10 +0100

    Sonderzeichen im DummyContent umgewandelt

--- content/index_sidebar.php
+++ content/index_sidebar.php
@@ -2,6 +2,6 @@
 			<p style="font-size:1.8em;line-height:1.3em;">Du bist Auszubildender in einem 
 			Medienorientierten Beruf?</p>
 			<p style="font-size:1.4em;line-height:1.3em;">Dann zeige uns Dein Talent und mach mit beim 
-			Designwettbewerb des Fritz-Henßler Förderpreis.</p>
+			Designwettbewerb des Fritz-Hen&szlig;ler F&ouml;rderpreis.</p>
 			<p style="font-size:1.4em;line-height:1.3em;">Es gibt grandiose Preise zu gewinnen.<br />
 			Faire Konkurrenz + Dein Fachgebiet  = Deine faire Chance.</p>
