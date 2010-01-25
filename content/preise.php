f2e2b4a4e353cdca547c058fa1db4f2b8a3643ca
zeidlos <julius@zeidlos.de>
2010-01-25 23:43:01 +0100

    Seite "Preise" hinzugefuegt. text-decoration:underline fuer a:hover hinzugefuegt. Formatierungen fuer Listen ins CSS eingepflegt.

---
+++ content/preise.php
@@ -0,0 +1,48 @@
+<h2>Preise</h2>
+<p>Dank unserer gro&szlig;&uuml;gigen Sponsoren k&ouml;nnen wir f&uuml;r die Gewinner des 
+Fritz-Hen&szlig;ler F&ouml;rderpreises einige sehr ansprechende Preise ausloben. 
+Es wird unter anderem folgende Preise zu gewinnen geben:
+</p>
+<p>
+	<ul>
+		<li>
+			F&uuml;r die 10 Gewinner jeder Kategorie gibt es einen 
+			hochwertigen Bildband mit den pr&auml;mierten Arbeiten.
+		</li>
+		<li>
+			Mehrt&auml;gige Schulungen inkl. &Uuml;bernachtung bei 
+			<a href="http://fhbk.de">Comcave/Dortmund.</a>
+		</li>
+		<li>
+			Ausstellungen in Dortmund und im Museum 
+			<a href="http://fhbk.de">"Camera Obscura"</a> in M&uuml;hlheim.
+		</li>
+		<li>
+			Sachpreise von <a href="http://bron.de">bron</a>, 
+			<a href="http://hp.com/">hp</a>, <a href="http://efi.com">efi</a>, 
+			<a href="http://sunbounce.com">Sunbounce</a> u.a.
+		</li>
+		<li>
+			Fotob&uuml;cher im Maximalformat von FujiFilm
+		</li>
+		<li>
+			Fineart Fotopapier von Hahnem&uuml;hle und ILFORD
+		</li>
+		<li>
+			Lehr- und Pr&uuml;fungsb&uuml;cher von Christiani und der ZFA
+		</li>
+	</ul>
+</p>
+<p>
+	Eine genaue Liste der Preise steht noch nicht fest, da weitere Sponsoren hinzukommen 
+	k&ouml;nnen. Es gibt keinen Anspruch auf einen bestimmten Preis. Einige Preise wird es nur
+	nur f&uuml;r die Gewinner in bestimmten Kategorien geben.
+</p>
+<p>
+	M&ouml;chten Sie Geld oder Sachpreise zur Verf&uuml;gung stellen und in die Liste der 
+	Sponsoren aufgenommen werden? Melden Sie sich unverbindlich bei 
+	<a href="mailto:wilmshoever@fhbk.de">wilmshoever@fhbk.de</a>
+</p>
+	
+	
+	
