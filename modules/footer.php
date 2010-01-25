8e58b511f9ad37f05807f2939a029adfee9f10ca
zeidlos <julius@zeidlos.de>
2010-01-25 21:03:14 +0100

    Footer auf Vordermann gebracht

--- modules/footer.php
+++ modules/footer.php
@@ -2,69 +2,84 @@
 	TODO Footerbereich aufr‰umen und Sponsorenlogos sinnvoll anordnen.
 -->
 <div class="container_24">
-	<div class="grid_24" style="background-color:rgb(42,42,42);">
+	<div class="grid_24 white">
+		<h4>Sponsoren</h4>
+	</div>
+	<div class="grid_24 footer">
 		<div class="grid_4">
-			<h4>Sponsoren</h4>
-			<a href="http://fhbk.de">
-				<img src="./img/content/sponsoren_logos/BronimagingfarbLogo.png" 
-				width="150" height="47" alt="BronimagingfarbLogo" />
-			</a>
-			<a href="http://fhbk.de">
-				<img src="./img/content/sponsoren_logos/CameraObscura_Logo.png" 
-				width="150" height="81" alt="CameraObscura Logo" />
-			</a>
+				<a href="http://fhbk.de">
+					<img src="./img/content/sponsoren_logos/BronimagingfarbLogo.png" 
+					width="130" alt="BronimagingfarbLogo" />
+				</a>
+				<br /><br />
+				<a href="http://fhbk.de">
+					<img src="./img/content/sponsoren_logos/CameraObscura_Logo.png" 
+					width="130" alt="CameraObscura Logo" />
+				</a>
 		</div>
 		<div class="grid_4">
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/COMCAVE_COLLEGE.png" 
-					width="150" height="150" alt="COMCAVE COLLEGE" />
+					width="130" alt="COMCAVE COLLEGE" />
 				</a>
 		</div>
 		<div class="grid_4">
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/christiani_transp_Logo.png" 
-					width="150" height="98" alt="Christiani Transp Logo" />
+					width="130" alt="Christiani Transp Logo" />
 				</a>
-				
+				<br /><br />				
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/FF-Imaging-Germany.png" 
-					width="150" height="48" alt="FF Imaging Germany" />
-				</a>
-				<a href="http://fhbk.de">
-					<img src="./img/content/sponsoren_logos/filmaxx_de.png" 
-					width="150" height="115" alt="Filmaxx De" />
+					width="130" alt="FF Imaging Germany" />
 				</a>
 		</div>
+		<div class="grid_4">
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/filmaxx_de_V2.png" 
-					width="150" height="19" alt="Filmaxx De V2" />
+					width="130" alt="Filmaxx De V2" />
 				</a>
+				<br /><br />
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/HFA-Logo_short-stripes-version.png" 
-					width="150" height="26" alt="HFA Logo Short Stripes Version" />
+					width="130" alt="HFA Logo Short Stripes Version" />
 				</a>
+				<br /><br />
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/ILFORD.png" 
-					width="150" height="35" alt="ILFORD" />
+					width="130" alt="ILFORD" />
 				</a>
-				<a href="http://fhbk.de">
-					<img src="./img/content/sponsoren_logos/nfa_s.png" 
-					width="150" height="30" alt="Nfa S" />
-				</a>
+				<br /><br />
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/schmidt-werden_logo.png" 
-					width="150" height="39" alt="Schmidt Werden Logo" />
+					width="130" alt="Schmidt Werden Logo" />
 				</a>
+		</div>
+		<div class="grid_4">
+				<a href="http://fhbk.de">
+					<img src="./img/content/sponsoren_logos/nfa_s.png" 
+					width="130" alt="Nfa S" />
+				</a>
+				<br /><br />
+				<a href="http://fhbk.de">
+					<img src="./img/content/sponsoren_logos/filmaxx_de.png" 
+					width="130" alt="Filmaxx De" />
+				</a>
+		</div>
+		<div class="grid_3">
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/sunbounce_sw.png" 
-					width="150" height="33" alt="Sunbounce Sw" />
+					width="130" alt="Sunbounce Sw" />
 				</a>
+				<br /><br />
 				<a href="http://fhbk.de">
 					<img src="./img/content/sponsoren_logos/ZFA_m_Zeile.png" 
-					width="150" height="81" alt="ZFA M Zeile" />
+					width="130" alt="ZFA M Zeile" />
 				</a>
 		</div>
 	</div>
 </div>
 <div class="clear"></div>
+<br />
 <p class="center">&copy; 2009 <a href="http://fhbk.de">Fritz-Henﬂler Berufskolleg</a></p>
+<br />
